624fb9a291934b3fcb423312eae4df41f40db3c3
ashiq <ashiqul@ergo-ventures.com>
2021-03-01 19:58:25 +0600

    views

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
 from odoo import models, fields, api, exceptions
 
 class Course(models.Model):
@@ -46,6 +47,8 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
+    color = fields.Integer()
+
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 
@@ -53,6 +56,12 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
 
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+
+    attendees_count = fields.Integer(string="Attendees count", compute='_get_attendees_count', store=True)
+
+
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -78,6 +87,34 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = r.start_date + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            r.duration = (r.end_date - r.start_date).days + 1
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
+
+
 
     @api.constrains('seats', 'attendee_ids')
     def _validate_seats(self):
