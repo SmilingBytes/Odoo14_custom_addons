251fd4a365603c0adbbef7b3dcfcbc9de6418479
ashiq <ashiqul@ergo-ventures.com>
2021-03-02 19:06:28 +0600

    Report and dashboard updated

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -22,7 +22,8 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
+
 
     # always loaded
     'data': [
@@ -32,8 +33,8 @@
         'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
-
-
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
